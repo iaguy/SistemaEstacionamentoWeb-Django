5620bbe1158617f1d117f10a4e757a8a88a0eecf
Bruno Iaguy <93688250+iaguy@users.noreply.github.com>
2022-06-14 18:25:21 -0300

    implementação do website(menu) e login

--- estacionamento/urls.py
+++ estacionamento/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('site.urls')),
+    path('', include('website.urls')),
     path('sistema/', include('core.urls')),
     path('admin/', admin.site.urls), 
     path('accounts/', include('django.contrib.auth.urls')),

9f15f3c6b063c45a3a83fa772d0123927f32ad80
Bruno Iaguy <93688250+iaguy@users.noreply.github.com>
2022-06-16 15:30:22 -0300

    finalização do website

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from .views import (
+    contato,
     home,
+    planos,
+    sobre,
+    servicos,
 )
 
 urlpatterns = [
     path('', home, name='website_home'),
+    path('contato/', contato, name='website_contato'),
+    path('planos/', planos, name='website_planos'),
+    path('sobre/', sobre, name='website_sobre'),
+    path('servicos/', servicos, name='website_servicos'),
 ]
